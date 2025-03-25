ef59eb3922990153e3f162c417bd6ec226bdb28b
MKudryash <129171406+Julia-Mamsheva@users.noreply.github.com>
2025-03-25 14:19:12 +0300

    Correct email

--- app/src/main/java/com/example/connectinternet/utils/isValidPassword.kt
+++ app/src/main/java/com/example/connectinternet/utils/isValidPassword.kt
@@ -3,7 +3,7 @@ package com.example.connectinternet.utils
 import android.text.TextUtils
 
 fun String.isEmailValid(): Boolean {
-    return !TextUtils.isEmpty(this) && Regex("^[a-z0-9]+@[a-z0-9]+\\.ru$").matches(this)
+    return !TextUtils.isEmpty(this) && Regex("^[a-z0-9]+@[a-z0-9]+\\.[a-z]{2,}$").matches(this)
 }
 
 fun String.isPasswordValid(): Boolean {
